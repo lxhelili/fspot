09bc88b6b803fabb279aafaa9ee6044fb714495a
Lavdim Xhelili <lavdimxhelili@gmail.com>
2019-09-01 23:32:59 +0200

    creating index.js file inside src folder

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello FlowrSpot</h1>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("app");
+ReactDOM.render(<App />, rootElement);
