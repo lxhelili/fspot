b20b2a24ef1695be8c5774b35d0aeeb6b314f2c3
Lavdim Xhelili <lavdimxhelili@gmail.com>
2019-09-03 19:09:47 +0200

    cover section added

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from './Cover.css';
+import Search from '../Search/Search';
 
 export default function Cover() {
     return (
         <div className={styles.cover}>
             <div className={styles.coverArea}>
-            <h1>Discover flowers around you</h1>
-            <p>Explore between more than 8.427 sightings</p>
+                <h1>Discover flowers around you</h1>
+                <p>Explore between more than 8.427 sightings</p>
+                <Search />
             </div>
         </div>
     )
---
+++ src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Search.css';
+
+export default function Search() {
+    return (
+        <div className={styles.inputGroup}>
+            <input type="text" placeholder="Looking for something specific?"/>
+            <button type="button"></button>
+        </div>
+    )
+}
