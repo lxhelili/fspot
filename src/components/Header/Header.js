8b5c2d0c4a888c9a07e9861e483ea97bf60646b5
Lavdim Xhelili <lavdimxhelili@gmail.com>
2019-09-18 01:00:49 +0200

    refactoring css styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as styles from './Header.module.css';
 import Logo from '../Logo/Logo';
 import Nav from '../Nav/Nav';
+import cx from 'classnames';
 
 export default function Header() {
+    const [menu, toggleMenu] = useState(false)
     return (
         <div className={styles.header}>
-            <div className={styles.left}>
-                <Logo/>
+            <div className={styles.headerLogo}>
+                <Logo />
+                <button className={styles.burgerMenu} type="button" onClick={() => toggleMenu(!menu)}></button>
             </div>
-            <div className={styles.right}>
-                <Nav/>
+            <div className={cx(styles.headerNav, (menu ? styles.active: ''))}>
+                <Nav />
             </div>
         </div>
     )
